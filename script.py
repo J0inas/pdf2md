4fd87c02f76b84e8a444a315d7537c3ad9987540
Joinas <jgoldhammer@outlook.de>
2024-03-30 11:11:20 +0100

    Add lecture number to process
    also did a little commenting of code

--- script.py
+++ script.py
@@ -3,38 +3,42 @@ from pdf2image import convert_from_path
 # this variable should not be changed in the process
 # md_path = "/Users/joinas/Documents/Obsidian/Life"
 md_path = "/Users/joinas/Documents/Uni/Semester 8/pdf2md/"
+# gets changed in the process
 pdf_name_and_path = ".pdf"
 lecture_title = "title"
-lecture_number = "0"
+lecture_number = ""
+# those are recommandations for the user to put in
 current_modules = {"PGP", "WR", "SiSy", "Chip-Krieg", "Frontend-Entwicklung"}
 
 # Use "input" for text input and create your new md file from images
 print("HELLO, welcome to //PDF2MD\\" + "\n" + "Please select your module")
 print(current_modules)
-module_name = "not a module" 
+module_name = "a module" 
 
-if (module_name not in current_modules):
-    module_name = input()
-    print("Very well then." + "\n")
+module_name = input()
+print("Very well then." + "\n")
     
 print("Please drag your file name into the console.")
 
 pdf_name_and_path = input()
-pdf_name_and_path.replace("'", "")
-
-pages = convert_from_path(pdf_name_and_path)
+pages = convert_from_path(pdf_name_and_path.replace("'", ""))
 
 for slides_num, page in enumerate(pages):
     page.save(f'{md_path}{module_name} {lecture_number} - {slides_num}.png', 'PNG')
     
-print ("File sucessfully turned into PNGs.")
+print ("File sucessfully turned into PNGs." + "\n")
 
-print ("\n" + "\n" + "Almost done now." + "\n")
+print ("Almost done now." + "\n")
 
-print("Please enter the name of the lecture:" + "\n")
+print ("Which lecture is this? Please enter a number like so '07'" + "\n")
 
-md_file_name = input ()
-md_path += "/" + lecture_number + " " + md_file_name + ".md"
+lecture_number = input()
+
+print("Oh and what is the name of the lecture? Please enter its name" + "\n")
+
+lecture_title = input ()
+
+md_path += f"/ {lecture_number} {lecture_title}.md"
 
 # open markdown file and write content:
 with open(md_path, 'w') as file:
